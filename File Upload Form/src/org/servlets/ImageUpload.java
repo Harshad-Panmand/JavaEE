38a34c3d87bc93a1e5006752c90a5330f8609620
Harshad Panmand <panmandharshad@gmail.com>
2020-03-22 11:10:33 +0530

    File upload program

---
+++ File Upload Form/src/org/servlets/ImageUpload.java
@@ -0,0 +1,45 @@
+package org.servlets;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+@WebServlet("/ImageUpload")
+public class ImageUpload extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		ServletFileUpload upload = new ServletFileUpload(new DiskFileItemFactory());
+		System.out.println("File will get uploaded to " + System.getProperty("java.io.tmpdir"));
+		try {
+			List<FileItem> files = upload.parseRequest(request);
+			for (FileItem file : files) {
+				// On windows getName returns full file path
+				String fileName = file.getName();
+				try {
+					fileName = fileName.substring(fileName.lastIndexOf("\\") + 1);
+				} catch (Exception e) {
+					System.out.println(e.getLocalizedMessage());
+				}
+
+				file.write(new File(System.getProperty("java.io.tmpdir") + fileName));
+				System.out.println(fileName);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
