e74e7c7dcad2af32e20bf0b8eed9be7d890a125b
Harshad Panmand <panmandharshad@gmail.com>
2020-04-04 20:18:37 +0530

    OAuth 2 for google

---
+++ GoogleOAuth/src/main/java/org/Oauth/model/UserInfo.java
@@ -0,0 +1,51 @@
+package org.Oauth.model;
+
+public class UserInfo {
+	private String id, email, picture;
+	private boolean verified_email;
+
+	public UserInfo() {
+		super();
+	}
+
+	public UserInfo(String id, String email, String picture, boolean verified_email) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.picture = picture;
+		this.verified_email = verified_email;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPicture() {
+		return picture;
+	}
+
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+
+	public boolean isVerified_email() {
+		return verified_email;
+	}
+
+	public void setVerified_email(boolean verified_email) {
+		this.verified_email = verified_email;
+	}
+
+}
